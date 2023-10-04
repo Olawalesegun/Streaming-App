6514415e25c5efe56f2b501161f739b707d6f7da
Olawalesegun <olawalesegun0@gmail.com>
2023-10-04 16:48:02 +0100

    email request

--- src/main/java/africa/semicolon/gemstube/dtos/request/EmailRequest.java
+++ src/main/java/africa/semicolon/gemstube/dtos/request/EmailRequest.java
@@ -1,4 +1,16 @@
 package africa.semicolon.gemstube.dtos.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Setter
+@Getter
 public class EmailRequest {
+    private Sender sender;
+    @JsonProperty("to")
+    private List<Recipient> recipients;
+    private String htmlContent;
+    private String subject;
 }
