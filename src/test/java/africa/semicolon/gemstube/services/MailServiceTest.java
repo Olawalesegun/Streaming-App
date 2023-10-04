491e267b4bee650121646394ca577d1d97d98a4b
Olawalesegun <olawalesegun0@gmail.com>
2023-10-04 16:44:09 +0100

    mail service test

--- src/test/java/africa/semicolon/gemstube/services/MailServiceTest.java
+++ src/test/java/africa/semicolon/gemstube/services/MailServiceTest.java
@@ -3,13 +3,20 @@ package africa.semicolon.gemstube.services;
 import africa.semicolon.gemstube.dtos.request.EmailRequest;
 import africa.semicolon.gemstube.dtos.request.Recipient;
 import africa.semicolon.gemstube.dtos.request.Sender;
+import africa.semicolon.gemstube.dtos.response.EmailResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 public class MailServiceTest {
+
+    @Autowired
+    private MailService mailService;
 
     @Test
     public void testSendEmail(){
@@ -21,8 +28,16 @@ public class MailServiceTest {
         recipient.setEmail("olawalesegun0@gmail.com");
         recipient.setName("OlaMoney");
         List<Recipient> recipients = List.of(recipient);
+
         EmailRequest emailRequest = new EmailRequest();
         emailRequest.setSender(sender);
         emailRequest.setRecipients(recipients);
+        emailRequest.setHtmlContent("" +
+                "<p>We are testing  our App</p>");
+        emailRequest.setSubject("testing 123");
+
+        EmailResponse response = mailService.sendMail(emailRequest);
+        assertNotNull(response);
+        assertNotNull(response.getMessageId());
     }
 }
