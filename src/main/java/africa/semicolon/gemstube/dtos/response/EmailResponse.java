61f2cd2e183351eb920a91e7006847707813c650
Olawalesegun <olawalesegun0@gmail.com>
2023-10-10 19:55:17 +0100

    mail sending with spring

--- src/main/java/africa/semicolon/gemstube/dtos/response/EmailResponse.java
+++ src/main/java/africa/semicolon/gemstube/dtos/response/EmailResponse.java
@@ -1,10 +1,13 @@
 package africa.semicolon.gemstube.dtos.response;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Setter
 @Getter
 public class EmailResponse {
     private String messageId;
+    private Integer code;
 }
