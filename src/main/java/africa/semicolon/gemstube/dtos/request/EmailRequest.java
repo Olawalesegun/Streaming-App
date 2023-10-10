ebe0e38d27b1e918b9a4fca80ab7cd071e98db48
Olawalesegun <olawalesegun0@gmail.com>
2023-10-10 19:54:13 +0100

    mail sending with spring

--- src/main/java/africa/semicolon/gemstube/dtos/request/EmailRequest.java
+++ src/main/java/africa/semicolon/gemstube/dtos/request/EmailRequest.java
@@ -1,7 +1,9 @@
 package africa.semicolon.gemstube.dtos.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -9,8 +11,14 @@ import java.util.List;
 @Getter
 public class EmailRequest {
     private Sender sender;
+
     @JsonProperty("to")
     private List<Recipient> recipients;
-    private String htmlContent;
+
     private String subject;
+    private String htmlContent;
+
+    public EmailRequest(){
+        this.sender = new Sender("gemstube@hotmail.africa", "Gemstube inc.");
+    }
 }
