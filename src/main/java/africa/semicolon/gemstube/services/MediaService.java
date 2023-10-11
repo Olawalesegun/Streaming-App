652d79a963c0955a492ffe113b132db329590f01
Olawalesegun <olawalesegun0@gmail.com>
2023-10-11 17:25:41 +0100

    implementation of upload functionality on our streaming site and it's test with Cloudinary(cloud) and media service

--- src/main/java/africa/semicolon/gemstube/services/MediaService.java
+++ src/main/java/africa/semicolon/gemstube/services/MediaService.java
@@ -1,2 +1,9 @@
-package africa.semicolon.gemstube.services;public interface MediaService {
+package africa.semicolon.gemstube.services;
+
+import africa.semicolon.gemstube.dtos.request.UploadMediaRequest;
+import africa.semicolon.gemstube.dtos.response.UploadMediaResponse;
+import africa.semicolon.gemstube.exceptions.GemsTubeException;
+
+public interface MediaService {
+    UploadMediaResponse upload(UploadMediaRequest uploadMediaRequest) throws GemsTubeException;
 }
