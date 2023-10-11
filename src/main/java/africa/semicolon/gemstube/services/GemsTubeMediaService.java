096e4ae0688930722bd5015d4a1d7578e01ec135
Olawalesegun <olawalesegun0@gmail.com>
2023-10-11 17:24:54 +0100

    implementation of upload functionality on our streaming site and it's test with Cloudinary(cloud) and media service

--- src/main/java/africa/semicolon/gemstube/services/GemsTubeMediaService.java
+++ src/main/java/africa/semicolon/gemstube/services/GemsTubeMediaService.java
@@ -1,2 +1,34 @@
-package africa.semicolon.gemstube.services;public class GemsTubeMediaService {
+package africa.semicolon.gemstube.services;
+
+import africa.semicolon.gemstube.dtos.request.UploadMediaRequest;
+import africa.semicolon.gemstube.dtos.response.UploadMediaResponse;
+import africa.semicolon.gemstube.exceptions.GemsTubeException;
+import africa.semicolon.gemstube.exceptions.MediaUploadException;
+import africa.semicolon.gemstube.repositories.MediaRepository;
+import africa.semicolon.gemstube.models.Media;
+import africa.semicolon.gemstube.models.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class GemsTubeMediaService implements MediaService{
+    private final CloudService cloudService;
+    private final MediaRepository mediaRepository;
+    private final UserService userService;
+    @Override
+    public UploadMediaResponse upload(UploadMediaRequest uploadMediaRequest) throws GemsTubeException {
+        User user = userService.getUserById(uploadMediaRequest.getCreatorId());
+        String fileUrl = cloudService.upload(uploadMediaRequest.getMultipartFile());
+        Media media = new Media();
+        media.setDescription(uploadMediaRequest.getDescription());
+        media.setUrl(fileUrl);
+        media.setTitle(uploadMediaRequest.getTitle());
+        media.setCreatedAt(LocalDateTime.now());
+        media.setUploader(user);
+        mediaRepository.save(media);
+        return null;
+    }
 }
