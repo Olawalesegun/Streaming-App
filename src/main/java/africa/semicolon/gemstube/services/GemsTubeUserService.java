7d7bc38fb000865877e09b7dffc254e42c1a0133
Olawalesegun <olawalesegun0@gmail.com>
2023-10-11 17:25:20 +0100

    implementation of upload functionality on our streaming site and it's test with Cloudinary(cloud) and media service

--- src/main/java/africa/semicolon/gemstube/services/GemsTubeUserService.java
+++ src/main/java/africa/semicolon/gemstube/services/GemsTubeUserService.java
@@ -4,6 +4,7 @@ import africa.semicolon.gemstube.dtos.request.EmailRequest;
 import africa.semicolon.gemstube.dtos.request.Recipient;
 import africa.semicolon.gemstube.dtos.request.RegisterRequest;
 import africa.semicolon.gemstube.dtos.response.RegisterResponse;
+import africa.semicolon.gemstube.exceptions.GemsTubeException;
 import africa.semicolon.gemstube.models.User;
 import africa.semicolon.gemstube.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -26,11 +27,18 @@ public class GemsTubeUserService implements UserService {
 
         //Trying to send this user a mail upon registering
         EmailRequest emailRequest = new EmailRequest();
-        emailRequest.setRecipients(List.of(new Recipient(savedUser.getEmail(), "")));
+        emailRequest.setRecipients(List.of(new Recipient(savedUser.getEmail(), "friend")));
         emailRequest.setHtmlContent("<p>Hi, welcome to gemstube.com, we are so...");
         emailRequest.setSubject("Welcome to Gemstube streaming service");
         mailService.sendMail(emailRequest);
 
         return new RegisterResponse(savedUser.getId());
+    }
+
+    @Override
+    public User getUserById(Long id) throws GemsTubeException {
+        return userRepository.findById(id).orElseThrow(
+                () -> new GemsTubeException(String.format("user with id %d not found", id))
+        );
     }
 }
