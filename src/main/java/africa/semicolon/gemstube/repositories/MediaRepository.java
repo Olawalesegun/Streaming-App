4e22dbde7417e95509e53fc08904dff7b708074c
nameisunknown <orangekapital@gmail.com>
2023-10-16 22:46:16 +0100

    mail sending with spring

--- src/main/java/africa/semicolon/gemstube/repositories/MediaRepository.java
+++ src/main/java/africa/semicolon/gemstube/repositories/MediaRepository.java
@@ -1,2 +1,7 @@
-package africa.semicolon.gemstube.repositories;public class MediaRepository {
+package africa.semicolon.gemstube.repositories;
+
+import africa.semicolon.gemstube.models.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaRepository extends JpaRepository<Media, Long> {
 }
