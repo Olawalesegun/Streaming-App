34572001400fbbae2d4961f56e229053330d9475
nameisunknown <orangekapital@gmail.com>
2023-10-16 22:45:20 +0100

    mail sending with spring

--- src/main/java/africa/semicolon/gemstube/models/Media.java
+++ src/main/java/africa/semicolon/gemstube/models/Media.java
@@ -1,2 +1,26 @@
-package africa.semicolon.gemstube.models;public class Media {
+package africa.semicolon.gemstube.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Media {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private String url;
+    @OneToOne(fetch = FetchType.EAGER)
+    private User uploader;
+    private LocalDateTime createdAt;
+
 }
