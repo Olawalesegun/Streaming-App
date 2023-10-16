7738fe6432e494d21ffc8b35cd80c4094f2a13ed
nameisunknown <orangekapital@gmail.com>
2023-10-16 22:39:02 +0100

    mail sending with spring

--- src/main/java/africa/semicolon/gemstube/dtos/request/UploadMediaRequest.java
+++ src/main/java/africa/semicolon/gemstube/dtos/request/UploadMediaRequest.java
@@ -1,2 +1,13 @@
-package africa.semicolon.gemstube.dtos.request;public class UploadMediaRequest {
+package africa.semicolon.gemstube.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+@Getter
+@Setter
+public class UploadMediaRequest {
+    private Long creatorId;
+    private MultipartFile multipartFile;
+    private String description;
+    private String title;
 }
