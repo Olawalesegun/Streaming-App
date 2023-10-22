a3298a33c472013df91a20dee4bb90daef088b50
nameisunknown <orangekapital@gmail.com>
2023-10-22 19:51:20 +0100

    comment implementation with group

--- src/main/java/africa/semicolon/gemstube/services/GemsTubeCommentService.java
+++ src/main/java/africa/semicolon/gemstube/services/GemsTubeCommentService.java
@@ -1,4 +1,25 @@
 package africa.semicolon.gemstube.services;
 
-public class GemsTubeCommentService {
+import africa.semicolon.gemstube.dtos.request.CreateCommentRequest;
+import africa.semicolon.gemstube.dtos.response.CreateCommentResponse;
+import africa.semicolon.gemstube.exceptions.GemsTubeException;
+import africa.semicolon.gemstube.models.Media;
+import africa.semicolon.gemstube.models.User;
+import africa.semicolon.gemstube.repositories.CommentRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.ui.Model;
+
+public class GemsTubeCommentService implements CommentService {
+    private final UserService userService;
+    private final MediaService mediaService;
+    private final CommentRepository commentRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public CreateCommentResponse createComment(CreateCommentRequest createCommentRequest) throws GemsTubeException {
+        User commenter = userService.getUserById(createCommentRequest.getCommenterId());
+        Media mediaToBeCommented = mediaService.getMediaById(createCommentRequest.getMediaId());
+
+        return null;
+    }
 }
