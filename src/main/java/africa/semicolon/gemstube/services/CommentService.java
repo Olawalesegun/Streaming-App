53c87dbadea21911a1735a2b28ac294303372aee
nameisunknown <orangekapital@gmail.com>
2023-10-22 19:50:51 +0100

    comment implementation with group

--- src/main/java/africa/semicolon/gemstube/services/CommentService.java
+++ src/main/java/africa/semicolon/gemstube/services/CommentService.java
@@ -1,2 +1,9 @@
-package africa.semicolon.gemstube.services;public class CommentService {
+package africa.semicolon.gemstube.services;
+
+import africa.semicolon.gemstube.dtos.request.CreateCommentRequest;
+import africa.semicolon.gemstube.dtos.response.CreateCommentResponse;
+import africa.semicolon.gemstube.exceptions.GemsTubeException;
+
+public interface CommentService {
+    CreateCommentResponse createComment(CreateCommentRequest createCommentRequest) throws GemsTubeException;
 }
