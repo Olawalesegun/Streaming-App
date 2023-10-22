953834ff6c5281b4d5810174d2824ffca4959fc7
nameisunknown <orangekapital@gmail.com>
2023-10-22 20:00:56 +0100

    comment implementation with group

--- src/main/java/africa/semicolon/gemstube/dtos/request/CreateCommentRequest.java
+++ src/main/java/africa/semicolon/gemstube/dtos/request/CreateCommentRequest.java
@@ -1,2 +1,14 @@
-package africa.semicolon.gemstube.dtos.request;public class CreateCommentRequest {
+package africa.semicolon.gemstube.dtos.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CreateCommentRequest {
+    private Long commenterId;
+    private Long mediaId;
+    private String message;
 }
