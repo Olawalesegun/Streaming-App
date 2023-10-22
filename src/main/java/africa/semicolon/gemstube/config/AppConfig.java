1569da9801e2137182ea5c489463667f643aed42
nameisunknown <orangekapital@gmail.com>
2023-10-22 20:08:41 +0100

    model mapper in appconfig

--- src/main/java/africa/semicolon/gemstube/config/AppConfig.java
+++ src/main/java/africa/semicolon/gemstube/config/AppConfig.java
@@ -1,6 +1,7 @@
 package africa.semicolon.gemstube.config;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
